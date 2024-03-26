3ed3b85c7f07c33b3e1da0d4a5be8a996ab184e9
AbdElRahmanOsama182 <bodyusama207@gmail.com>
2024-03-26 06:39:44 +0200

    CMake

---
+++ build/CMakeFiles/MatrixMul.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixMul.dir/home/abdelrahman/Desktop/Matrix-Multiplication/MatrixMul.c.o"
+  "CMakeFiles/MatrixMul.dir/home/abdelrahman/Desktop/Matrix-Multiplication/MatrixMul.c.o.d"
+  "MatrixMul"
+  "MatrixMul.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MatrixMul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
